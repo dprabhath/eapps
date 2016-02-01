823b6527bbf7c3ba63a6842fe71ab7e90cf8422a
Prabhath Mannapperuma <d.p.mannapperuma@gmail.com>
2016-02-01 10:43:20 +0530

    dd

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    app_id = ENV['app_id']
-    secret = ENV['secret']
+    //app_id = ENV['app_id']
+    //secret = ENV['secret']
+	app_id = 895562953865919
+	secret = a7f72b0ede9646a3046dd7d2e9f7c158
+	
+	
+	
 
     provider :facebook, app_id, secret, scope: 'publish_actions',
                 :image_size => {width: 500, height: 500}
