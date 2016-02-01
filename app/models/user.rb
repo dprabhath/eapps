e4e4235b1888b180c94ed45d76c91a3f0740a9b5
Prabhath Mannapperuma <d.p.mannapperuma@gmail.com>
2016-02-01 13:05:44 +0530

    dddd

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
     images = Overlay.images
     images.map! { |image| "#{uid}_#{image}" }
     s3 = Aws::S3::Resource.new(
-            region:'us-east-1',
+            region:'US Standard',
             credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'],
                                           ENV['AWS_SECRET_ACCESS_KEY']))
     bucket = s3.bucket(ENV['S3_BUCKET_NAME'])
