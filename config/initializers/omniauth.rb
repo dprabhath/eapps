29f056f5ac1cc870cc56fd3e440190b4c58576da
Prabhath Mannapperuma <d.p.mannapperuma@gmail.com>
2016-02-01 11:00:41 +0530

    id added

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,6 +6,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 	
 	
 
-    provider :facebook, app_id, secret, scope: 'publish_actions',
+    provider :facebook, '895562953865919', 'a7f72b0ede9646a3046dd7d2e9f7c158', scope: 'publish_actions',
                 :image_size => {width: 500, height: 500}
 end
