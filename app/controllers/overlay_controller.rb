565a6de2d47c675ef9eb1b34023146fa8659ff45
Prabhath Mannapperuma <d.p.mannapperuma@gmail.com>
2016-02-01 12:19:38 +0530

    ddd

--- app/controllers/overlay_controller.rb
+++ app/controllers/overlay_controller.rb
@@ -41,7 +41,7 @@ class OverlayController < ApplicationController
   private
     def fetch_image
       image = params.fetch(:image) if params[:image]
-      url = 'https://s3.amazonaws.com/eappsxyz'
+      url = 'https://eappsxyz.s3.amazonaws.com'
       @file_path = "#{url}/#{current_user.uid}_#{image}.png"
     end
 
