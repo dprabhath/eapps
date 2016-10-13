5e77c6b3766e00287bc0c7bcce6386ca662791d7
Prabhath Mannapperuma <it14121098@my.sliit.lk>
2016-10-13 10:02:43 +0530

    app id changed

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,6 +6,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 	
 	
 
-    provider :facebook, '770027773143418', '22464fb63efa613db51a00f6fac2bc71', scope: 'publish_actions',
+    provider :facebook, '931427906945547', '476720ad69544b3a5df495479f4c6f21', scope: 'publish_actions',
                 :image_size => {width: 500, height: 500}
 end
